23d2e051d663b4ae5104240d1661c4ebfb91f158
shellby.miller <shellby.miller@gmail.com>
2023-02-03 19:17:42 -0500

    updated Post model

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+//import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+
+//import our database connection from config.js
+const sequelize = require('../config/connection');
+
+//initialize Post model by extending off Sequelize's Model class
+class Post extends Model {}
+
+//set up fields and rules for Post model
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post'
+    }
+  );
+  
+  module.exports = Post;
