2252583624416e4990faaf2fd04cddf0447dc96a
shellby.miller <shellby.miller@gmail.com>
2023-02-24 13:50:05 -0500

    added comment view and js

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -3,8 +3,8 @@ const { Comment } = require('../models');
 const commentData = [
     {
         comment_text: "It's not often that beauty and genius come in such a nice package.",
-        user_id: 2,
-        post_id: 3,
+        user_id: 1,
+        post_id: 2,
         
     },
     {
