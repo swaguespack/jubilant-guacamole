5e10991c0adb2d2bf880ad315f19fc86d256bdbd
shellby.miller <shellby.miller@gmail.com>
2023-02-24 18:00:57 -0500

    edits to comment view

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,12 +1,10 @@
 async function commentFormHandler(event) {
     event.preventDefault();
 
-    const post_id = document.querySelector('input[name="post-id').value;
-    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-
-   /* const post_id = window.location.toString().split('/')[
+    const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
-    ];*/
+    ];    
+    const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
 
     if (comment_text) {
         const response = await fetch('/api/comments', {
